facbe63c1736d18c72a3117370e6a786cdb4948f
heimdallr <heimdalrnsk@gmail.com>
2026-06-11 10:47:27 +0700

    Парсер списка участников команд из wiki

--- src/home/PlayerParser/PlayerParser.cmake
+++ src/home/PlayerParser/PlayerParser.cmake
@@ -1,4 +1,5 @@
 AddTarget(PlayerParser app_console
 	PROJECT_GROUP    Tool
 	SOURCE_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+	LINK_LIBRARIES Qt${QT_MAJOR_VERSION}::Core
 )
--- src/home/PlayerParser/PlayerParser.cpp
+++ src/home/PlayerParser/PlayerParser.cpp
@@ -1,9 +1,36 @@
-#include <filesystem>
-#include <fstream>
+
 #include <iostream>
 #include <set>
 #include <stdexcept>
-#include <string>
+
+#include <QDir>
+#include <QRegularExpression>
+#include <QString>
+#include <QTextStream>
+
+namespace
+{
+
+constexpr const char* MONTHS[] { "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" };
+
+constexpr std::pair<const char*, const char*> TYPES[] {
+	{		"Вратари",      "Вратарь" },
+	{	  "Защитники",     "Защитник" },
+	{  "Полузащитники", "Полузащитник" },
+	{     "Нападающие",   "Нападающий" },
+	{ "Главный тренер",       "Тренер" }
+};
+
+QString ParseBirthday(const QString& str)
+{
+	static QRegularExpression rx("^(\\d{1,2}) (.*?) (\\d{4})$");
+	const auto                match = rx.match(str);
+	assert(match.hasMatch());
+	const auto it = std::ranges::find(MONTHS, match.captured(2));
+	assert(it != std::end(MONTHS));
+	auto result = QString("%1.%2.%3").arg(match.captured(3)).arg(std::distance(std::begin(MONTHS), it) + 1, 2, 10, QChar { '0' }).arg(match.captured(1).toInt(), 2, 10, QChar { '0' });
+	return result;
+}
 
 struct Player
 {
@@ -11,26 +38,17 @@ public:
 	using List = std::set<Player>;
 
 public:
-	int         Id;
-	std::string Country, Type, Name, Birthday;
-
-	Player(const int id, const std::filesystem::path& path, std::string type, std::string name, std::string birthday)
-		: Id { id }
-		, Country { path.filename().string() }
-		, Type { std::move(type) }
-		, Name { std::move(name) }
-		, Birthday { std::move(birthday) }
-	{
-	}
+	int     id;
+	QString country, type, name, birthday;
 
 	bool operator<(const Player& rhs) const noexcept
 	{
-		return Id < rhs.Id;
+		return id < rhs.id;
 	}
 
 	std::ostream& Write(std::ostream& stream) const
 	{
-		return stream << std::format("insert into t_player_buf(country, player_type, num, name, birthday) values('{}', '{}', {}, '{}', '{}');\n", Country, Type, Id, Name, Birthday);
+		return stream << QString("insert into t_player_buf(country, player_type, num, name, birthday) values('%1', '%2', %3, '%4', '%5');\n").arg(country, type).arg(id).arg(name, birthday).toStdString();
 	}
 };
 
@@ -39,72 +57,63 @@ std::ostream& operator<<(std::ostream& stream, const Player& player)
 	return player.Write(stream);
 }
 
-bool GetLine(std::istream& stream, std::string& buf)
+Player GetPlayer(QString country, QString type, const QRegularExpressionMatch& match)
 {
-	std::string s;
-
-	while (s.empty())
-	{
-		if (!std::getline(stream, s))
-			return false;
-
-		s.erase(std::ranges::find(s, '\t'), std::end(s));
-
-		s.erase(
-			std::find_if(
-				s.rbegin(),
-				s.rend(),
-				[](const char ch) {
-					return true && ch != ' ' && ch != '\n' && ch != '\r' && ch != '\t';
-				}
-			).base(),
-			s.end()
-		);
-
-		if (s == "-")
-			s.clear();
-
-		buf.clear();
-		for (const auto c : s)
-		{
-			buf.push_back(c);
-			if (c == '\'')
-				buf.push_back(c);
-		}
-	}
-
-	return true;
+	return { .id = match.captured(1).toInt(), .country = std::move(country), .type = std::move(type), .name = match.captured(2), .birthday = ParseBirthday(match.captured(3)) };
 }
+
+Player GetCoach(QString country, QString type, const QRegularExpressionMatch& match)
+{
+	return { .id = -1, .country = std::move(country), .type = std::move(type), .name = match.captured(1), .birthday = ParseBirthday(match.captured(2)) };
+}
+
+using Parser = Player (*)(QString, QString, const QRegularExpressionMatch&);
+
+} // namespace
 
 int main(const int argc, char* argv[])
 {
 	try
 	{
-		if (argc < 2)
+		if (argc < 2) [[unlikely]]
 			throw std::invalid_argument("usage:\nPlayerParser path_to_folder_with_players");
 
-		for (const auto& entry : std::filesystem::directory_iterator(argv[1]))
+		QDir srcDir(argv[1]);
+		if (!srcDir.exists()) [[unlikely]]
+			throw std::invalid_argument(std::format("{} not exists", argv[1]));
+
+		const std::pair<QRegularExpression, Parser> parsers[] {
+			{   QRegularExpression("^(\\d*?)\t(.*?)\t.*?\t(.*?)\t.*?$"), &GetPlayer },
+			{ QRegularExpression(QString("^\\D*?\t(.*?)\t(.*?)\t.*?$")),  &GetCoach },
+		};
+
+		for (const auto& fileName : srcDir.entryList({}, QDir::Files))
 		{
-			Player::List  players;
-			std::string   type;
-			std::ifstream inp(entry.path());
-			std::string   buf;
-			while (GetLine(inp, buf))
+			QFile file(srcDir.filePath(fileName));
+			if (!file.open(QIODevice::ReadOnly)) [[unlikely]]
+				throw std::invalid_argument(std::format("Cannot read {}", fileName.toStdString()));
+
+			Player::List players;
+			QString      type;
+			QTextStream  stream(&file);
+			while (true)
 			{
-				std::string name, birthday;
-				if (!std::ranges::all_of(std::as_const(buf), [](const char c) {
-						return c >= '0' && c <= '9';
-					}))
+				const auto str = stream.readLine();
+				if (str.isEmpty())
+					break;
+
+				if (const auto it = std::ranges::find(TYPES, str, &std::pair<const char*, const char*>::first); it != std::end(TYPES))
 				{
-					type = std::move(buf);
+					type = it->second;
 					continue;
 				}
-				const auto id = std::stoi(buf);
-				GetLine(inp, name);
-				GetLine(inp, birthday);
-				GetLine(inp, buf); // club
 
-				players.emplace(id, entry.path(), type, std::move(name), std::move(birthday));
+				for (const auto& [rx, parser] : parsers)
+					if (const auto match = rx.match(str); match.hasMatch())
+					{
+						players.emplace(std::invoke(parser, fileName, type, match));
+						break;
+					}
 			}
 
 			for (const auto& player : players)
